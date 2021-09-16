e4890aef4033f68ff254d947c64a02c4c95dfa83
Caden <cjgud882@leaner.dvusd.org>
2021-09-16 09:26:55 -0700

    Car Movement(With Boosting), Turret Movement, Basic Trigger Detection
    - Added Basic trigger detection using OnTriggerEnter
    - Added physics based arcade car movement
    - Added Turret look at cursor
    
    Caden Gudenkauf

---
+++ CarGame/Assets/_Scripts/_GameMode/winMode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winMode : MonoBehaviour
+{
+
+    
+
+    public void OnTriggerEnter(Collider other)
+    {
+       
+        if(other.gameObject.tag=="endZone")
+        {
+            Debug.Log("Entered Delivery Zone");
+
+        }
+        
+    }
+
+
+    
+
+
+}
